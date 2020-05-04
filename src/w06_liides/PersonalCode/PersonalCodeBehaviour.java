ccfa27b78ab3199f4bfcaa06fa06aae12e746306
andre <andrku@tlu.ee>
2020-05-04 20:40:37 +0300

    Personal code (implements, exdends)

---
+++ src/w06_liides/PersonalCode/PersonalCodeBehaviour.java
@@ -0,0 +1,13 @@
+package PersonalCode;
+
+/**
+ * PersonalCode.PersonalCodeBehaviour
+ */
+public interface PersonalCodeBehaviour {
+
+    public int getGender();
+    public String getGenderWord();
+    public int getYear();
+    public int getMonth();
+    public int getDay();
+}
